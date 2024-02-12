d8810d9bce57ceefefe3b6463ef89798dc3cb11d
Zvonimir Lokmer <lokmer@gmail.com>
2024-02-12 20:19:46 +0100

    added configureBrowsershot() static method on the driver to configure Browsershot instance before rendering PDF

--- src/Drivers/BrowsershotDriver.php
+++ src/Drivers/BrowsershotDriver.php
@@ -7,6 +7,12 @@ use Spatie\Browsershot\Browsershot;
 
 class BrowsershotDriver implements Driver
 {
+    protected static $configurator = null;
+
+    public static function configureBrowsershot(callable $configurator): void {
+        static::$configurator = $configurator;
+    }
+
     public function getData(Pdfable $pdf): ?string
     {
         $html = $pdf->getView()->render();
@@ -17,56 +23,12 @@ class BrowsershotDriver implements Driver
             ->margins(...$page->getMargins());
 
         // Configure browsershot instance
-        $this->configureBrowsershot($browser);
+        if (isset(self::$configurator)) {
+            $browser = call_user_func(self::$configurator, $browser);
+        }
 
         return base64_decode(
             $browser->base64pdf()
         );
-    }
-
-    protected function configureBrowsershot(Browsershot $browserShot)
-    {
-        if (filled(config('pdfable.browsershot.chrome_path'))) {
-            $browserShot->setChromePath(
-                config('pdfable.browsershot.chrome_path')
-            );
-        }
-
-        if (filled(config('pdfable.browsershot.chromium_arguments'))) {
-            $browserShot->addChromiumArguments(
-                config('pdfable.browsershot.chromium_arguments')
-            );
-        }
-
-        if (filled(config('pdfable.browsershot.node_binary'))) {
-            $browserShot->setNodeBinary(
-                config('pdfable.browsershot.node_binary')
-            );
-        }
-
-        if (filled(config('pdfable.browsershot.npm_binary'))) {
-            $browserShot->setNpmBinary(
-                config('pdfable.browsershot.npm_binary')
-            );
-        }
-
-        if (filled(config('pdfable.browsershot.include_path'))) {
-            $browserShot->setIncludePath(
-                config('pdfable.browsershot.include_path')
-            );
-        }
-
-        if (filled(config('pdfable.browsershot.node_modules_path'))) {
-            $browserShot->setNodeModulePath(
-                config('pdfable.browsershot.node_modules_path')
-            );
-        }
-
-        if (filled(config('pdfable.browsershot.options'))) {
-            collect(config('pdfable.browsershot.options'))
-                ->each(
-                    fn ($item, $key) => $browserShot->setOption($key, $item)
-                );
-        }
     }
 }
